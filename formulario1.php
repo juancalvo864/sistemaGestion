b33f175a0e58e3b37cbe0417dcc96e348d17c57b
Juan <juan.calvo.dev@gmail.com>
2023-12-05 13:23:54 -0300

    correccion ultimo commit

--- formulario1.php
+++ formulario1.php
@@ -1,31 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/style.css">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
         $fecha_actual = date("l/m/Y");
         $texto = '<div class="comentContainer">'.
                     '<div class="name_surname_msj">'.
-                        "<h3> Nombre: </h3>" . "<p>" . $_POST['nombre'] . "</p>" .
+                        "<h3> Nombre: </h3>" . "<p>" .$_POST['nombre'] . "</p>" .
                     "</div>".
                     '<div class="name_surname_msj">'.
-                        "<h3> Apellido: </h3>" ."<p>" . $_POST['apellido'] ."</p>" .
+                        "<h3> Apellido: </h3>" ."<p>" .$_POST['apellido'] ."</p>" .
                     "</div>".
                     '<div class="name_surname_msj">'.
                         "<h3> Mensaje: </h3>" ."<p>" .$_POST['msj']. "</p>" . 
                     "</div>".
-                    "<h5>" . "Fecha del mensaje: ". $fecha_actual . "</h5>".
+                    "<h5>" . "Fecha del mensaje: ".$fecha_actual . "</h5>".
                  "</div>";
-
-        $archivo = fopen("mensajes.txt", "a");
+        $archivo = fopen("mensajes.txt","a");
         fputs($archivo, $texto);
-
+        fclose($archivo);
         header("Location:formulario.php?ok");
-    ?>
-</body>
-</html>
+?>
